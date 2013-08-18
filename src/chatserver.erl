33bcadb526d5dca14445d415fb51a5f7a43680b0
Josh Adams <josh@isotope11.com>
2013-08-17 22:12:29 -0500

    Add #part functionality.

--- src/chatserver.erl
+++ src/chatserver.erl
@@ -1,9 +1,12 @@
 -module(chatserver).
 -behaviour(gen_server).
--export([start_link/0, join/2, nicklist/1]).
--export([init/1, handle_call/3, handle_info/2,
+-export([start_link/0, join/2, nicklist/1, part/1]).
+-export([init/1, handle_call/3, handle_info/2, handle_cast/2,
                  terminate/2, code_change/3]).
 -include("chatserver_records.hrl").
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 
 %%% Client API
 start_link() ->
@@ -15,24 +18,31 @@ join(Pid, Username) ->
 nicklist(Pid) ->
     gen_server:call(Pid, nicklist).
 
+part(Pid) ->
+    gen_server:call(Pid, part).
+
 %%% Server functions
 init(State) -> {ok, State}. %% no treatment of info here!
 
 handle_call({join, Username}, From, #state{}=State) ->
-    io:format("~p~n", [State#state.listeners]),
-    NewUser = #user{username=Username, pid=From},
-    NewState = State#state{listeners=[NewUser|State#state.listeners]},
+    NewState = add_user(Username, From, State),
     {reply, ok, NewState};
 handle_call(nicklist, _From, #state{}=State) ->
     {reply, list_usernames(State), State};
+handle_call(part, From, #state{}=State) ->
+    NewState = remove_user(From, State),
+    {reply, ok, NewState};
 handle_call({send, Message}, _From, #state{}=State) ->
     {reply, { ok, Message }, State}.
+
+handle_cast(_Ref, _Request) ->
+    io:format("No idea", []).
 
 handle_info(Msg, State) ->
     io:format("Unexpected message: ~p~n",[Msg]),
     {noreply, State}.
 
-terminate(normal, State) ->
+terminate(normal, _State) ->
     io:format("terminating..."),
     ok.
 
@@ -47,3 +57,12 @@ list_usernames(State) ->
         X#user.username
     end,
     lists:map(MapToUsername, State#state.listeners).
+
+add_user(Username, {FromPid, _Ref}, State) ->
+    NewUser = #user{username=Username, pid=FromPid},
+    State#state{listeners=[NewUser|State#state.listeners]}.
+
+remove_user({FromPid, _Ref}, State) ->
+    OldListeners = State#state.listeners,
+    NewListeners = lists:filter(fun(User) -> User#user.pid =/= FromPid end, OldListeners),
+    State#state{listeners=NewListeners}.
--- test/unit/chatserver_test.erl
+++ test/unit/chatserver_test.erl
@@ -11,20 +11,24 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
 join_test_() ->
-    {"A client can connect to the server, which adds its User to the listeners list.",
+    {"A client can connect to the server, which adds its User to the listeners list. [gen_server protocol]",
      ?setup(fun can_connect/1)}.
 
 nicklist_test_() ->
-    {"A client can list the connected users.",
+    {"A client can list the connected users. [gen_server protocol]",
      ?setup(fun can_list_users/1)}.
 
 send_test_() ->
-    {"A client can send messages.",
+    {"A client can send messages. [gen_server protocol]",
      ?setup(fun can_send_message/1)}.
 
 join_and_appear_in_nicklist_test_() ->
     {"A client sees itself in the nicklist after joining.",
      ?setup(fun can_connect_and_then_see_self_in_nicklist/1)}.
+
+part_and_disappear_from_nicklist_test_() ->
+    {"A client that disconnects no longer shows up in the nicklist.",
+     ?setup(fun can_disconnect_and_username_is_removed_from_nicklist/1)}.
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% Setup Functions %%%
@@ -45,23 +49,29 @@ stop(_) ->
 %%%%%%%%%%%%%%%%%%%%
 %%% Actual Tests %%%
 %%%%%%%%%%%%%%%%%%%%
-can_connect(Self) ->
+can_connect(SelfPid) ->
     InitialState = #state{listeners=[], messages=[]},
-    Knewter = #user{username="knewter", pid=Self},
+    Knewter = #user{username="knewter", pid=SelfPid},
     StateWithUser = #state{listeners=[Knewter], messages=[]},
-    [?_assertMatch({reply, ok, StateWithUser}, chatserver:handle_call({join, "knewter"}, Self, InitialState))].
+    [?_assertMatch({reply, ok, StateWithUser}, chatserver:handle_call({join, "knewter"}, {SelfPid, []}, InitialState))].
 
-can_list_users(Self) ->
-    Knewter = #user{username="knewter", pid=Self},
+can_list_users(SelfPid) ->
+    Knewter = #user{username="knewter", pid=SelfPid},
     StateWithUser = #state{listeners=[Knewter], messages=[]},
-    [?_assertMatch({reply, ["knewter"], StateWithUser}, chatserver:handle_call(nicklist, Self, StateWithUser))].
+    [?_assertMatch({reply, ["knewter"], StateWithUser}, chatserver:handle_call(nicklist, {SelfPid, []}, StateWithUser))].
 
-can_send_message(Self) ->
-    Knewter = #user{username="knewter", pid=Self},
+can_send_message(SelfPid) ->
+    Knewter = #user{username="knewter", pid=SelfPid},
     StateWithUser = #state{listeners=[Knewter], messages=[]},
-    [?_assertMatch({reply, { ok, "some message" }, StateWithUser}, chatserver:handle_call({send, "some message" }, Self, StateWithUser))].
+    [?_assertMatch({reply, { ok, "some message" }, StateWithUser}, chatserver:handle_call({send, "some message" }, SelfPid, StateWithUser))].
 
-can_connect_and_then_see_self_in_nicklist(Self) ->
+can_connect_and_then_see_self_in_nicklist(_) ->
     {ok, Pid} = chatserver:start_link(),
     chatserver:join(Pid, "knewter"),
     [?_assertEqual(["knewter"], chatserver:nicklist(Pid))].
+
+can_disconnect_and_username_is_removed_from_nicklist(_) ->
+    {ok, Pid} = chatserver:start_link(),
+    chatserver:join(Pid, "knewter"),
+    chatserver:part(Pid),
+    [?_assertEqual([], chatserver:nicklist(Pid))].
