2f71765b0e2aadda7deacbdf9977310aaae28e4b
Josh Adams <josh@isotope11.com>
2013-08-25 19:16:50 -0500

    Fix PORT assignment for heroku

--- src/chromechat_app.erl
+++ src/chromechat_app.erl
@@ -4,20 +4,38 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-define(C_ACCEPTORS, 100).
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    Dispatch = cowboy_router:compile([
-            {'_', [
-                    {"/", toppage_handler, []},
-                    {"/websocket", ws_handler, []}
-                ]}
-        ]),
-    {ok, _} = cowboy:start_http(http, 100, [{port, 8000}], % so this will start an http server on port 8000, and the routes are up there ^ ^ ^
-        [{env, [{dispatch, Dispatch}]}]),
+    Routes = routes(),
+    Dispatch = cowboy_router:compile(Routes),
+    Port = port(),
+    TransOpts = [{port, Port}],
+    ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+    {ok, _} = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
     chromechat_sup:start_link().
 
 stop(_State) ->
     ok.
+
+routes() ->
+    [
+        {'_', [
+                {"/", toppage_handler, []},
+                {"/websocket", ws_handler, []}
+            ]}
+    ].
+
+port() ->
+    case os:getenv("PORT") of
+        false ->
+            {ok, Port} = application:get_env(http_port),
+            Port;
+        Other ->
+            {Integer, _} = string:to_integer(Other),
+            Integer
+    end.
